63c620db7e8830fccf6511b862b0c1ddd735095d
Ivan <ivandasilvamartin1@gmail.com>
2024-01-19 19:01:24 +0000

    un guardado

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Instanciacion
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -10,6 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Configuraciones
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,12 +20,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')))
 
+// rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// manejador de errores
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,5 +44,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Export del servidor
 
 module.exports = app;
